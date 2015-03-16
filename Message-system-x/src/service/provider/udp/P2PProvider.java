133da2316b836df6e7e2e02f5d634fb7b9a6c3ea
Unikum <haris@standout.se>
2015-03-16 08:32:20 +0100

    WIP Java doc. P2PProvider done

--- Message-system-x/src/service/provider/udp/P2PProvider.java
+++ Message-system-x/src/service/provider/udp/P2PProvider.java
@@ -21,43 +21,47 @@ import java.util.Random;
  */
 public class P2PProvider implements IServiceProvider {
 
-    private boolean listening = false;
-    private DatagramSocket receiveSocket;
-    private DatagramSocket sendSocket;
-    private Thread listenThread;
-    private Thread sendThread;
+    // variables
+    private boolean _listening = false;
+    private DatagramSocket _receiveSocket;
+    private DatagramSocket _sendSocket;
+    private String _ip;
+    private int _port;
+    private Thread _listenThread;
+    private Thread _sendThread;
+    private String _listenEndPoint;
+    private String _sendEndPoint;
+    private int _sequenceCounter = 0;
+    private int _sequenceRangeIndex = 0;
 
-    private String listenEndPoint;
-    private String sendEndPoint;
-
-    private int sequenceCounter = 0;
-    private int sequenceRangeIndex = 0;
-
+    // constants
     private final static String STOP_LISTENING = "/stop";
+    private final static String ADDRESS_SEPARATOR = ":";
 
+    /**
+     * This method creates a separate thread on which it awaits
+     * an incoming request.
+     * @param endPoint Uses endpoint to create DatagramSocket with port
+     * @param messageReceiver Provided to use onMessage to receive message from server.
+     * @throws Exception
+     */
     @Override
     public void startListening(final String endPoint, final IMessageReceiver messageReceiver) throws Exception {
 
-        listenEndPoint = endPoint;
-        String[] ad = extractConnection(listenEndPoint);
-        final String listenIp = ad[0];
-        final int listenPort = Integer.parseInt(ad[1]);
+        _listenEndPoint = endPoint;
+        extractConnection(_listenEndPoint);
 
-        listening = true;
+        _listening = true;
 
-        listenThread = new Thread(new Runnable() {
+        _listenThread = new Thread(new Runnable() {
             @Override
             public void run() {
-
                 try {
-
                     // Bind socket to endpoint and port
-                    receiveSocket = new DatagramSocket(listenPort);
+                    _receiveSocket = new DatagramSocket(_port);
 
                     // Listen loop
-                    while(listening) {
-                        // Waiting for packets
-
+                    while(_listening) {
                         // Init receive buffer
                         byte[] data = new byte[4];
 
@@ -65,7 +69,7 @@ public class P2PProvider implements IServiceProvider {
                         DatagramPacket receivePacket = new DatagramPacket(data, data.length );
 
                         // Receiving packet and storing in buffer
-                        receiveSocket.receive(receivePacket);
+                        _receiveSocket.receive(receivePacket);
                         // Received packet, sending back to whatever...
 
                         int len = 0;
@@ -76,11 +80,11 @@ public class P2PProvider implements IServiceProvider {
 
                         byte[] buffer = new byte[len];
                         receivePacket = new DatagramPacket(buffer, buffer.length );
-                        receiveSocket.receive(receivePacket);
+                        _receiveSocket.receive(receivePacket);
 
-                        ByteArrayInputStream baos = new ByteArrayInputStream(buffer);
-                        ObjectInputStream oos = new ObjectInputStream(baos);
-                        Message message = (Message)oos.readObject();
+                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(buffer);
+                        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+                        Message message = (Message)objectInputStream.readObject();
 
                         Message newMessage = new Message(
                                 message.getName(),
@@ -93,79 +97,60 @@ public class P2PProvider implements IServiceProvider {
 
                 } catch (Exception ex) {
                     ex.printStackTrace();
-                    receiveSocket = null;
+                    _receiveSocket = null;
                 }
             }
         });
 
-        listenThread.start();
+        _listenThread.start();
     }
 
+    /**
+     * Stop listening to incoming messages.
+     * Sending a message with disconnection to endpoint.
+     * @throws Exception
+     */
     @Override
     public void stopListening() throws Exception {
-        listening = false;
+        _listening = false;
         sendMessage(new Message(
                 null,
                 STOP_LISTENING,
-                receiveSocket.getInetAddress().getHostAddress(),
-                MessageType.SINGLE) ,listenEndPoint);
+                _receiveSocket.getInetAddress().getHostAddress(),
+                MessageType.SINGLE), _listenEndPoint);
     }
 
+    /**
+     * Creates a separate thread which is used to communicate with the
+     * other peer endpoint.  Emulates interruptions in message sent.
+     * @param message
+     * @param destinationEndPoint
+     */
     @Override
     public void sendMessage(final Message message, final String destinationEndPoint) {
 
-        sendEndPoint = destinationEndPoint;
-        String[] ad = extractConnection(destinationEndPoint);
-        final String sendIp = ad[0];
-        final int sendPort = (Integer.parseInt(ad[1]));
+        _sendEndPoint = destinationEndPoint;
+        extractConnection(destinationEndPoint);
 
-        sendThread = new Thread(new Runnable() {
+        _sendThread = new Thread(new Runnable() {
             @Override
             public void run() {
 
                 try {
                     String newMessage = message.getMessage();
 
-                    if(Messenger.messageDelay > 0)
-                        Thread.currentThread().sleep(Messenger.messageDelay);
-
-                    if(Messenger.messageLoss) {
-
-                        Random random = new Random();
-                        int first = random.nextInt(message.getMessage().length());
-                        String split = message.getMessage().substring(0, first);
-                        int second = random.nextInt(split.length() <= 0 ? 1 : split.length());
-                        int bound = split.length()-second;
-                        newMessage = message.getMessage().substring(bound <= 0 ? 1 : bound);
-                    }
-
-                    if(Messenger.sequenceLoss && !message.getMessageType().equals(MessageType.SINGLE)) {
-
-                        sequenceCounter++;
-
-                        if(sequenceCounter <= Messenger.sequenceLimit) {
-                            if(sequenceCounter == Messenger.sequenceLimit) {
-                                sequenceCounter = 0;
-                                sequenceRangeIndex = sequenceRangeIndex == 0 ? 1 : 0;
-                            }
-
-                            int failureRate = Messenger.sequenceRange[sequenceRangeIndex];
-                            int roll = (int)(Math.random() * 100);
-
-                            if((roll - failureRate) <= 0)
-                                return;
-                        }
-                    }
+                    // Emulates interruptions depending on configuration
+                    newMessage = emulateInterruption(message, newMessage, Thread.currentThread());
 
                     // Write object to stream
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    ObjectOutputStream oos = new ObjectOutputStream(baos);
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
                     //oos.writeObject(message);
-                    oos.writeObject(new Message(message.getName(), newMessage, message.getEndPoint(), message.getMessageType()));
-                    oos.flush();
+                    objectOutputStream.writeObject(new Message(message.getName(), newMessage, message.getEndPoint(), message.getMessageType()));
+                    objectOutputStream.flush();
 
                     // get the byte array of the object
-                    byte[] buf = baos.toByteArray();
+                    byte[] buf = byteArrayOutputStream.toByteArray();
 
                     int number = buf.length;
                     byte[] data = new byte[4];
@@ -177,32 +162,74 @@ public class P2PProvider implements IServiceProvider {
                     }
 
                     // Bind the socket to the destinationEndPoint and port
-                    sendSocket = new DatagramSocket();
+                    _sendSocket = new DatagramSocket();
 
-                    DatagramPacket sendPacket = new DatagramPacket(data, 4, InetAddress.getByName(sendIp), sendPort);
+                    DatagramPacket sendPacket = new DatagramPacket(data, 4, InetAddress.getByName(_ip), _port);
 
                     // Send the object size
-                    sendSocket.send(sendPacket);
+                    _sendSocket.send(sendPacket);
 
                     // now send the object
-                    sendPacket = new DatagramPacket(buf, buf.length, InetAddress.getByName(sendIp), sendPort);
-                    sendSocket.send(sendPacket);
+                    sendPacket = new DatagramPacket(buf, buf.length, InetAddress.getByName(_ip), _port);
+                    _sendSocket.send(sendPacket);
 
                     // Send is complete, close the socket
-                    sendSocket.close();
-                    sendSocket = null;
+                    _sendSocket.close();
+                    _sendSocket = null;
 
                 } catch(Exception ex) {
                     ex.printStackTrace();
-                    sendSocket.close();
-                    sendSocket = null;
+                    _sendSocket.close();
+                    _sendSocket = null;
                 }
             }
         });
-        sendThread.start();
+        _sendThread.start();
     }
 
-    private String[] extractConnection(String endPoint) {
-        return endPoint.split(":");
+    /**
+     * This method is a helper to extract address to separate ip and port.
+     * @param endPoint The incoming full address
+     */
+    private void extractConnection(String endPoint) {
+        _ip = endPoint.split(ADDRESS_SEPARATOR)[0];
+        _port = Integer.parseInt(endPoint.split(ADDRESS_SEPARATOR)[1]);
+    }
+
+    /**
+     * Emulates interruption using configuration from messengerBundle, loaded in Configuration.
+     */
+    public String emulateInterruption(Message message, String newMessage, Thread currentThread) throws InterruptedException {
+        if(Messenger.messageDelay > 0)
+            currentThread.sleep(Messenger.messageDelay);
+
+        if(Messenger.messageLoss) {
+
+            Random random = new Random();
+            int first = random.nextInt(message.getMessage().length());
+            String split = message.getMessage().substring(0, first);
+            int second = random.nextInt(split.length() <= 0 ? 1 : split.length());
+            int bound = split.length()-second;
+            newMessage = message.getMessage().substring(bound <= 0 ? 1 : bound);
+        }
+
+        if(Messenger.sequenceLoss && !message.getMessageType().equals(MessageType.SINGLE)) {
+
+            _sequenceCounter++;
+
+            if(_sequenceCounter <= Messenger.sequenceLimit) {
+                if(_sequenceCounter == Messenger.sequenceLimit) {
+                    _sequenceCounter = 0;
+                    _sequenceRangeIndex = _sequenceRangeIndex == 0 ? 1 : 0;
+                }
+
+                int failureRate = Messenger.sequenceRange[_sequenceRangeIndex];
+                int roll = (int)(Math.random() * 100);
+
+                if((roll - failureRate) <= 0)
+                    return newMessage;
+            }
+        }
+        return newMessage;
     }
 }
